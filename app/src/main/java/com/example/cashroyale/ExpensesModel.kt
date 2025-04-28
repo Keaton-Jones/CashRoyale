2dae35684cff2b64e484acd5a76ad8586d6fea4f
lab_services_student <st10291916@vcconnect.edu.za>
2025-04-28 12:26:12 +0200

    Expenses model and MainActivity binding

---
+++ app/src/main/java/com/example/cashroyale/ExpensesModel.kt
@@ -0,0 +1,7 @@
+package com.example.cashroyale
+
+data class Expense(
+    val description: String,
+    val amount: Double,
+    val date: String
+)
--- app/src/main/java/com/example/cashroyale/MainActivity.kt
+++ app/src/main/java/com/example/cashroyale/MainActivity.kt
@@ -1,24 +1,35 @@
 package com.example.cashroyale
 
 // Kotlin
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.cashroyale.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
         navHostFragment?.let {
             val navController = it.navController
-            NavigationUI.setupWithNavController(bottomNavigationView, navController)
+            }
+            binding.btnExpenses.setOnClickListener {
+                val intent = Intent(this, Expenses::class.java)
+                startActivity(intent)
+            }
         }
     }
-}
